4c0a90bc61b24a68600b6924e0f7163bffbdc12c
Luciano Lamónica <lucho_bio@hotmail.com>
2019-04-04 00:27:04 -0300

    Agregando componente Cronometro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JuegoComponent } from './components/juego/juego.component';
+import { CronometroComponent } from './components/cronometro/cronometro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JuegoComponent
+    JuegoComponent,
+    CronometroComponent
   ],
   imports: [
     BrowserModule,
