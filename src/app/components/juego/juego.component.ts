c491edda0e6e81f4d4a74532a72f7b0684d00f0e
Luciano Lamónica <lucho_bio@hotmail.com>
2019-04-03 22:33:36 -0300

    Colocando minas en el tablero y los contadores a sus alrrededores

--- src/app/components/juego/juego.component.ts
+++ src/app/components/juego/juego.component.ts
@@ -11,14 +11,17 @@ export class JuegoComponent implements OnInit {
   public tablero: Celda[][]; // Tablero de celdas
   public tam: number; // Tamaño del tablero
   public estadoDeJuego: number; // 0 Jugando; 1 Game Over; 2 Victoria
-  
+  public minas: number;
+
   constructor() {
     this.tablero = [];
     this.tam = 8;
     this.estadoDeJuego = 0;
-   }
+    this.minas = 10;
+  }
 
   ngOnInit() {
+    this.crearTablero();
   }
 
   // Metodo que inicializa las celdas del tablero
@@ -29,6 +32,38 @@ export class JuegoComponent implements OnInit {
         this.tablero[fila][columna] = new Celda(false, false, false, 0);
       }
     }
+  }
+
+  // Metodo utilizado para colocar las minas en el tablero
+  colocarMinas() {
+    for (var mina = 0; mina < this.minas; mina++) {
+
+      var fila, columna;
+
+      do {
+        fila = Math.floor((Math.random() * 8)); // Numero aleatorio para las filas
+        columna = Math.floor((Math.random() * 8)); // Numero aleatorio para las columnas
+      } while (this.tablero[fila][columna].esMina == true); // Permite que solo se coloque una mina en la celda
+
+      this.tablero[fila][columna].esMina = true; // Coloco mina
+
+      // Agregamos a las celdas la cantidad de minas que hay a su alrrededor
+      for (var fila2 = this.max(0, fila - 1); fila2 < this.min(this.tam, fila + 2); fila2++) {
+        for (var columna2 = this.max(0, columna - 1); columna2 < this.min(this.tam, columna + 2); columna2++) {
+          if (this.tablero[fila2][columna2].esMina == false) { // Si la celda no es una bomba
+            this.tablero[fila2][columna2].probabilidad++; // Incrementa el contador de minas
+          }
+        }
+      }
+    }
+  }
+
+  // Metodos utilizados para no pasarse de los bordes del tablero
+  max(a, b) {
+    return Math.max(a, b);
+  }
+  min(a, b) {
+    return Math.min(a, b);
   }
 
 }
