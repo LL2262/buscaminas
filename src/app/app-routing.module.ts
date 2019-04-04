21fbc35b19ad06a696786d2b053f890d7cebcc5b
Luciano Lamónica <lucho_bio@hotmail.com>
2019-04-03 22:46:02 -0300

    Agregando rutas y empezando con maquetado front end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { JuegoComponent } from './components/juego/juego.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: JuegoComponent},
+  {path:'juego', component: JuegoComponent},
+  {path:'**', component: JuegoComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
