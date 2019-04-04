b0a1baa45c8c2c0c54389ed4b5b68f223858db69
Luciano Lamónica <lucho_bio@hotmail.com>
2019-04-04 00:35:42 -0300

    Mostrando cronometro en juego

--- src/app/components/cronometro/cronometro.component.ts
+++ src/app/components/cronometro/cronometro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cronometro } from 'src/app/modelos/cronometro';
 
 @Component({
   selector: 'app-cronometro',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CronometroComponent implements OnInit {
 
-  constructor() { }
+  public cronometro: Cronometro;
+  public intervalo;
+
+  constructor() {
+    this.cronometro = new Cronometro(0, 0, 0);
+   }
 
   ngOnInit() {
+    this.start();
+  }
+
+  start(){
+    this.intervalo = 0;
+    this.intervalo = setInterval(()=>{
+      this.cronometro.segundos +=1;
+      if(this.cronometro.segundos == 60){
+        this.cronometro.segundos = 0;
+        this.cronometro.minutos +=1;
+        if(this.cronometro.minutos == 60){
+          this.cronometro.minutos = 0;
+          this.cronometro.horas +=1;
+          if(this.cronometro.horas == 24){
+            this.cronometro.horas = 0;
+          }
+        }
+      }
+    }, 1000);
   }
 
 }
---
+++ src/app/modelos/cronometro.ts
@@ -0,0 +1,7 @@
+export class Cronometro{
+    constructor(
+        public horas: number,
+        public minutos: number,
+        public segundos: number, 
+    ){}
+}
